04588f7a298ed4c4e3ca020871d6487fcdcccde1
Zorawar <44931523+zorofrizzy@users.noreply.github.com>
2018-11-11 02:28:46 +0530

    Create bluetoothString.ino
    Copy/paste in Arduino IDE and save the project alternatively clone it.

---
+++ bluetoothString.ino
@@ -0,0 +1,27 @@
+#include<SoftwareSerial.h>
+
+//Create an object named bt of the class SoftwareSerial 
+SoftwareSerial bt(0,1);     // (Rx,Tx) pins defined  
+
+void setup() 
+{
+  bt.begin(9600);    // Define baud rate for software serial communication 
+  Serial.begin(9600);     // Define baud rate for serial communication 
+
+}
+
+
+
+
+void loop() {
+  //char input[10];       /* The char array was an alternative but here we would be stuck with limited number of characters.*/
+  String input;
+    if (bt.available())
+      { 
+      input=bt.readString();
+    
+    //Serial.println(input); 
+    //Print character received on to the serial monitor
+    //When connected to a smartphone, one can actually transmit data using this code with ease.
+    }
+}
